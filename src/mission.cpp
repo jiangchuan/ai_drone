70d0141f5c250c0158c6782f01a68ce72ae84955
jiangchuan <jiangchuan.huang@gmail.com>
2018-09-17 21:33:38 -0500

    Added local_pos_callback

--- src/mission.cpp
+++ src/mission.cpp
@@ -12,6 +12,7 @@
 #include <mavros_msgs/State.h>
 
 #include "sensor_msgs/Imu.h"
+#include "sensor_msgs/NavSatFix.h"
 // #include <sensor_msgs/LaserScan.h>
 #include "tf/tf.h"
 #include <angles/angles.h>
@@ -47,13 +48,23 @@ void state_callback(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
 }
 
+void local_pos_callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+    ROS_INFO("Local Pos Seq: [%d]", msg->header.seq);
+    ROS_INFO("Local Pos Position x: [%f], y: [%f], z: [%f]", msg->pose.position.x,msg->pose.position.y,msg->pose.position.z);
+    ROS_INFO("Local Pos Orientation x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.orientation.x,msg->pose.orientation.y,msg->pose.orientation.z,msg->pose.orientation.w);
+}
+
 void imu_callback(const sensor_msgs::Imu::ConstPtr& msg) {
     current_qtn_msg = msg->orientation;
-    // ROS_INFO("Hello");
     // ROS_INFO("Imu Seq: [%d]", msg->header.seq);
     // ROS_INFO("Imu Orientation x: [%f], y: [%f], z: [%f], w: [%f]", msg->orientation.x,msg->orientation.y,msg->orientation.z,msg->orientation.w);
     // ROS_INFO("Imu Angular Velocity roll: [%f], pitch: [%f], yaw: [%f]", msg->angular_velocity.x,msg->angular_velocity.y,msg->angular_velocity.z);
     // ROS_INFO("Imu Linear Acceleration x: [%f], y: [%f], z: [%f]", msg->linear_acceleration.x,msg->linear_acceleration.y,msg->linear_acceleration.z);
+}
+
+void gps_callback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
+    ROS_INFO("GPS Seq: [%d]", msg->header.seq);
+    ROS_INFO("GPS latitude: [%f], longitude: [%f], altitude: [%f]", msg->latitude,msg->longitude,msg->altitude);
 }
 
 double x = 0.0, y = 0.0, z = 0.0;
@@ -173,8 +184,14 @@ int main(int argc, char **argv)
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
             ("mavros/state", 10, state_callback);
 
+    ros::Subscriber local_pos_sub = nh.subscribe<geometry_msgs::PoseStamped>
+            ("/mavros/local_position/pose", 10, local_pos_callback);
+
     ros::Subscriber imu_sub = nh.subscribe<sensor_msgs::Imu>
           ("mavros/imu/data", 100, imu_callback);
+
+    ros::Subscriber gps_sub = nh.subscribe<sensor_msgs::NavSatFix>
+          ("mavros/global_position/global", 100, gps_callback);
 
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
             ("mavros/setpoint_position/local", 10);
@@ -222,27 +239,29 @@ int main(int argc, char **argv)
     land_cmd.request.longitude = 0;
     land_cmd.request.altitude = 0;
 
-    ros::Time last_request = ros::Time::now();
-
     // change to offboard mode and arm
-    while(ros::ok() && !current_state.armed) {
-        if( current_state.mode != "OFFBOARD" &&
-            (ros::Time::now() - last_request > ros::Duration(5.0))){
-          ROS_INFO(current_state.mode.c_str());  // Sometimes this does not show, and the drone does not fly.
+    ros::Time last_request = ros::Time::now();
+    while(ros::ok() && current_state.mode != "OFFBOARD"){
+        if(ros::Time::now() - last_request > ros::Duration(5.0)) {
+            ROS_INFO(current_state.mode.c_str());
             if( set_mode_client.call(offb_set_mode) &&
-                offb_set_mode.response.mode_sent){
-                ROS_INFO("Offboard enabled"); // Sometimes this does not show, and the drone does not fly.
+                offb_set_mode.response.mode_sent) {
+                ROS_INFO("Offboard enabled");
             }
             last_request = ros::Time::now();
-        } else {
-            if( !current_state.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( arming_client.call(arm_cmd) &&
-                    arm_cmd.response.success){
-                    ROS_INFO("Vehicle armed");
-                }
-                last_request = ros::Time::now();
+        }
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    while(ros::ok() && !current_state.armed){
+        if (ros::Time::now() - last_request > ros::Duration(5.0)) {
+            if( arming_client.call(arm_cmd) &&
+                arm_cmd.response.success) {
+                ROS_INFO("Vehicle armed");
             }
+            last_request = ros::Time::now();
         }
         local_pos_pub.publish(pose);
         ros::spinOnce();
@@ -461,5 +480,6 @@ int main(int argc, char **argv)
       ros::spinOnce();
       rate.sleep();
     }
+
     return 0;
 }
